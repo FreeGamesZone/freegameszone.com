acda00f345e84274cc7e20a880c6cfba97b49d1b
FreeGamesZone <ar2838591@gmail.com>
2026-01-20 21:38:33 +0500

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,5 @@
-let openGame =null;
-function openGame(path) {
-    window.open(path, "_blank");
-}
+// Make sure function is accessible globally
+window.openGame = function(gameName) {
+    alert(`${gameName} is now loading!`);
+    // Future: You can replace this with actual game opening logic
+};
