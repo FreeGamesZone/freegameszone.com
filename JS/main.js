c7f868d9631784df16255ec31f7ab171ae4d57b0
FreeGamesZone <ar2838591@gmail.com>
2026-01-21 18:13:01 +0500

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,9 @@
-// Make sure function is accessible globally
-window.openGame = function(gameName) {
-    alert(`${gameName} is now loading!`);
-    // Future: You can replace this with actual game opening logic
-};
+function searchGames() {
+  let input = document.getElementById("searchInput").value.toLowerCase();
+  let cards = document.getElementsByClassName("card");
+
+  for (let i = 0; i < cards.length; i++) {
+    let title = cards[i].getElementsByTagName("h3")[0].innerText.toLowerCase();
+    cards[i].style.display = title.includes(input) ? "block" : "none";
+  }
+}
